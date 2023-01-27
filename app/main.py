a053084af888d2a505ac5139c80dc035aa6eafc1
disxpanda <disxpanda@ej_ra.localdomain>
2023-01-27 06:59:09 -0600

    update main to include graph route.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/graph/')
+def graph_html():
+  return render_template('generatedgraph.html')
 
 if __name__ == '__main__':
   app.run()
